523d540a38f14de69a2fb391437f2bd7196d119e
sakibkibria <sakibkibria@gmail.com>
2020-08-26 22:53:37 -0400

    second commit

--- lib/main.dart
+++ lib/main.dart
@@ -50,7 +50,7 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 100;
+  int _counter = 1000;
 
   void _incrementCounter() {
     setState(() {
